332fa2c721af256c08456e538b988ec168e621d2
Burnsedia <bburnsed1995@gmail.com>
2026-02-10 17:23:56 -0500

    setting up tailwind

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,9 +1,21 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
+import vue from '@astrojs/vue'
+import { VitePWA } from '@vite-pwa/astro'
 
-import vue from '@astrojs/vue';
-
-// https://astro.build/config
 export default defineConfig({
-  integrations: [vue()]
-});
+  integrations: [
+    vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'BoomerBill',
+        short_name: 'BoomerBill',
+        start_url: '/app',
+        display: 'standalone',
+        background_color: '#111111',
+        theme_color: '#111111'
+      }
+    })
+  ]
+})
+
