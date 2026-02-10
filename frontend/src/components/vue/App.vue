5bbe03a4e76f1e1ec4568b6c9dd9e3852978f75e
Burnsedia <bburnsed1995@gmail.com>
2026-02-10 17:18:45 -0500

    building out thjis app

---
+++ frontend/src/components/vue/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <main>
+    <h2>BoomerBill App</h2>
+    <RateInput />
+    <Timer />
+    <Totals />
+    <Leaderboard />
+  </main>
+</template>
+
+<script setup>
+import RateInput from './components/RateInput.vue'
+import Timer from './components/Timer.vue'
+import Totals from './components/Totals.vue'
+import Leaderboard from './components/Leaderboard.vue'
+</script>
---
+++ frontend/src/components/vue/RateInput.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref, watch } from 'vue'
+
+const rate = ref(localStorage.getItem('boomerbill_rate') || 75)
+
+watch(rate, (val) => {
+  localStorage.setItem('boomerbill_rate', val)
+})
+</script>
+
+<template>
+  <label>
+    Hourly Rate ($)
+    <small>(default: avg US developer)</small>
+  </label>
+  <input type="number" v-model="rate" />
+</template>
