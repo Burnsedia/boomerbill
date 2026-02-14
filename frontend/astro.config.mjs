bbd2ec8b9cf050b48459bf4ff61dc9e2ab96b6c4
Burnsedia <bburnsed1995@gmail.com>
2026-02-14 08:27:49 -0500

    fix: add pwa icons

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -16,9 +16,22 @@ export default defineConfig({
         name: 'BoomerBill',
         short_name: 'BoomerBill',
         start_url: '/app',
+        scope: '/',
         display: 'standalone',
         background_color: '#111111',
-        theme_color: '#111111'
+        theme_color: '#111111',
+        icons: [
+          {
+            src: '/icons/icon-192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/icons/icon-512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
       }
     })
   ]
