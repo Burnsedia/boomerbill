bc0cfc65b97827f06bffb74e1b42b0f065b2aa97
Burnsedia <bburnsed1995@gmail.com>
2026-03-06 01:00:11 -0500

    working on the backend

--- backend/core/boomers/models.py
+++ backend/core/boomers/models.py
@@ -1,3 +1,32 @@
 from django.db import models
-
+from django.contrib.auth import get_user_model()
 # Create your models here.
+User = get_user_model()
+
+class Boomer(models.Model):
+    id = models.UUIDField
+    name = models.CharField
+    
+    def __str__(self):
+        return self.name
+
+class Category(models.Model):
+    id = models.CharField
+    name = models.CharField
+    is_defualt = models.BooleanField
+    
+    def __str__(self):
+        return self.name
+
+class Session(models.Model):
+    owner = models.ForeignKey(User,  on_delete=CASCADE, related_name="Victom")
+    boomer = models.ForeignKey(Boomer, on_delete=CASCADE, related_name="boomerSession")
+    category = models.ForeignKey(Category, on_delete=CASCADE, related_name="boomerHelpType")
+    #NOTE: Cost in Time in seconds
+    minutes = models.IntegerField(on_delete=CASCADE, related_name="boomerHelpTime")
+    #NOTE: Cost in penies 
+    cost = models.IntegerField(on_delete=CASCADE, related_name="boomerHelpTime")
+    start = models.DateTimeField(on_delete=CASCADE, related_name="boomerHelpStart")
+    end = models.DateTimeField(on_delete=CASCADE, related_name="boomerHelpEnd")
+    note = models.TextField(on_delete=CASCADE, related_name="boomerHelpNotes")
+
--- backend/core/boomers/views.py
+++ backend/core/boomers/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
-
+from rest_framework import permissions
+from rest_framework import renderers
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
 # Create your views here.
+from boomers.models import Boomer, Category, Session
+from boomers.permissions import IsOwnerOrReadOnly 
+
+class CategoryViewSet(ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+
+class BoomerViewSet(ModelViewSet):
+    queryset = Boomer.objects.all()
+    serializer_class = BoomerSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+
+class SessionViewSet(ModelViewSet):
+    queryset =  Session.objects.all()
+    serializer_class = SessionSerializer
+    permission_classes = [permissions.IsAuthenticated, IsOwnerOrReadOnly]
