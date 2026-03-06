71532fb822eabf40901341f7b4f899244f2f443b
Burnsedia <bburnsed1995@gmail.com>
2026-03-06 01:21:15 -0500

    fix(boomers/models): fixed syntax errors

--- backend/core/boomers/models.py
+++ backend/core/boomers/models.py
@@ -1,32 +1,33 @@
+import uuid
 from django.db import models
-from django.contrib.auth import get_user_model()
-# Create your models here.
+from django.contrib.auth import get_user_model
+
 User = get_user_model()
 
 class Boomer(models.Model):
-    id = models.UUIDField
-    name = models.CharField
-    
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=255)
+
     def __str__(self):
         return self.name
 
 class Category(models.Model):
-    id = models.CharField
-    name = models.CharField
-    is_defualt = models.BooleanField
-    
+    id = models.CharField(max_length=100, primary_key=True)
+    name = models.CharField(max_length=255)
+    is_default = models.BooleanField(default=False)
+
     def __str__(self):
         return self.name
 
 class Session(models.Model):
-    owner = models.ForeignKey(User,  on_delete=CASCADE, related_name="Victom")
-    boomer = models.ForeignKey(Boomer, on_delete=CASCADE, related_name="boomerSession")
-    category = models.ForeignKey(Category, on_delete=CASCADE, related_name="boomerHelpType")
-    #NOTE: Cost in Time in seconds
-    minutes = models.IntegerField(on_delete=CASCADE, related_name="boomerHelpTime")
-    #NOTE: Cost in penies 
-    cost = models.IntegerField(on_delete=CASCADE, related_name="boomerHelpTime")
-    start = models.DateTimeField(on_delete=CASCADE, related_name="boomerHelpStart")
-    end = models.DateTimeField(on_delete=CASCADE, related_name="boomerHelpEnd")
-    note = models.TextField(on_delete=CASCADE, related_name="boomerHelpNotes")
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="victim_sessions")
+    boomer = models.ForeignKey(Boomer, on_delete=models.CASCADE, related_name="boomer_sessions")
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="category_sessions")
 
+    # NOTE: Cost in time (seconds)
+    minutes = models.IntegerField()
+    # NOTE: Cost in pennies
+    cost = models.IntegerField()
+    start = models.DateTimeField()
+    end = models.DateTimeField()
+    note = models.TextField(blank=True)
